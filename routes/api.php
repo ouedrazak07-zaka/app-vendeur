fefd96432287f858c81921fa99daa89a13c0b2dd
Zakaria Ouedraogo <ton-email@exemple.com>
2026-09-06 09:26:47 +0000

    Retrait de l'authentification API (usage mono-utilisateur)

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,9 @@ use App\Http\Controllers\VenteDetailController;
 use App\Http\Controllers\VenteGrosController;
 use App\Http\Controllers\DebitCreditController;
 use App\Http\Controllers\InventaireController;
-use App\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('commandes', CommandeController::class);
-    Route::apiResource('ventes-detail', VenteDetailController::class);
-    Route::apiResource('ventes-gros', VenteGrosController::class);
-    Route::apiResource('debit-credit', DebitCreditController::class);
-    Route::apiResource('inventaires', InventaireController::class);
-});
+Route::apiResource('commandes', CommandeController::class);
+Route::apiResource('ventes-detail', VenteDetailController::class);
+Route::apiResource('ventes-gros', VenteGrosController::class);
+Route::apiResource('debit-credit', DebitCreditController::class);
+Route::apiResource('inventaires', InventaireController::class);
